d55b7bc7e7d6badbef4c0583179dc155ea83e52b
Len Smith <len@barrison.com>
2013-07-01 20:52:13 -0400

    Memoize the Let

--- lib/let-unit.rb
+++ lib/let-unit.rb
@@ -2,7 +2,14 @@ module Test
   module Unit
     class TestCase
       def self.let(x, &block)
-        define_method x, block
+        define_method x do
+          ivar_key = "@___#{x}"
+          unless instance_variable_defined?(ivar_key)
+            value = yield block
+            instance_variable_set(ivar_key, value)
+          end
+          instance_variable_get(ivar_key)
+        end
       end
     end
   end
--- test/test_let-unit.rb
+++ test/test_let-unit.rb
@@ -1,9 +1,16 @@
 require 'helper'
 
 class TestLetUnit < Test::Unit::TestCase
-  let(:one) { 1 }
+  let(:one)  { 1 }
+  let(:time) { Time.now }
 
   should "allow let statements" do
     assert_equal one, 1
+  end
+
+  should "memoize the let" do
+    current = time
+    assert_equal current, time
+    assert_not_equal current, one
   end
 end
